025a4d02f073ca9e3e7d0fe00f13fb4fbc7c5c15
Ubaid Delawala <delawalaubaid@gmail.com>
2021-09-15 16:12:26 -0400

    initial commit

---
+++ .history/index_20210915160421.ts
@@ -0,0 +1,43 @@
+import http from 'http';
+import path from 'path';
+import fs from 'fs';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+fs.readFile("index.html", (err, data)=>
+{
+  if(err)
+  {
+    console.log("Error")
+  }
+
+  console.log(data);
+});
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!');
+});
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+function displayHome(res: http.ServerResponse): void
+{
+  fs.readFile("index.html", (err, data)=>
+{
+  if(err)
+  {
+    res.writeHead(404);
+    res.end("Error 404: Page not found");
+    console.log("Error");
+  }
+
+  res.writeHead(200);
+  res.end(data);
+});
+}
